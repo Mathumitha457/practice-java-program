d5fd6de150bfb585ee08494d69e7fbec17dfc928
Mathumitha457 <s.mathumitha457@gmail.com>
2025-08-31 21:14:37 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,17 @@
+ public class datatype {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		if(a>b && a>c) {
+			System.out.println("true");
+		}
+		else {
+			System.out.println("false");
+		}
+			
+	}
+
+}
+ 
+
