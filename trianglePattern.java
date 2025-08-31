bbf5e4971f52997bbc7ef921b5de7fff8d25c5e1
Mathumitha457 <s.mathumitha457@gmail.com>
2025-08-31 21:21:43 +0530

    Add files via upload

---
+++ trianglePattern.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+public class trianglePattern {
+	public static void main(String[]args) {
+		Scanner src = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n= src.nextInt();
+		for(int row=1;row<=n;row++) {
+			for(int col=1;col<=row;col++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		src.close();
+	}
+}
+			
+				 
+				 
+    
+
