e402432f089c9842883aa3162641530fb0d7f22d
Mathumitha457 <s.mathumitha457@gmail.com>
2025-08-31 21:13:45 +0530

    Add files via upload

---
+++ arthimetic.java
@@ -0,0 +1,17 @@
+
+public class arthimetic {
+	public static void main(String [] args) {
+		int a=50; char b='a';
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		
+	}
+
+}
+ 
+    
+
