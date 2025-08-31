7a86201f56dfca7f9664004b8173efbc7c34b29c
Mathumitha457 <s.mathumitha457@gmail.com>
2025-08-31 21:15:21 +0530

    Add files via upload

---
+++ divisible.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+public class divisible {
+	public static void main(String[] args) {
+		Scanner src=new Scanner(System.in);
+		System.out.println("ENTER THE NUMBER");
+		int i=src.nextInt();
+		if(i%3==0 && i%5==0) {
+			System.out.println("divisible by 3 and 5");
+		}
+		else {
+			System.out.println("not divisible by 3 and 5");
+		}
+			src.close();
+		}
+	}
+
+
+ 
+    
+
