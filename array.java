9932c768389f174a52926c3895f9a95744bea78d
Mathumitha457 <s.mathumitha457@gmail.com>
2025-09-11 21:04:03 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        Scanner src=new Scanner(System.in);
+        System.out.println("enter the value");
+        int size=src.nextInt();
+        int arr[] =new int[3];
+        for (int i=0;i<size;i++) {
+            arr[i]=src .nextInt();
+        }
+            for(int i=0;i<size;i++){
+                System.out.print(arr[i] +" ");
+
+            }
+        
+            
+        }}
+
+
+        
+    
+    
+
