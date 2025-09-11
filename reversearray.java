ac4181d501b24f817cfe4767c4f652ce04126c9c
Mathumitha457 <s.mathumitha457@gmail.com>
2025-09-11 21:08:13 +0530

    Add files via upload

---
+++ reversearray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class reversearray{
+public static void main(String[] args) {
+        Scanner src=new Scanner(System.in);
+        System.out.println("enter the value");
+        int size=src.nextInt();
+        int arr[] =new int[3];
+        for (int i=0;i<size;i++) {
+            arr[i]=src .nextInt();
+        }
+            for(int i=size-1;i>=0;i--){
+                System.out.print(arr[i] +" ");
+
+            }
+            src.close();
+        
+            
+        }}
+    
+
