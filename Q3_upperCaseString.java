187c3dca5784be3c3dda994bbfe3a9c28bda24ba
Mathumitha457 <s.mathumitha457@gmail.com>
2025-10-03 11:17:20 +0530

    Add files via upload

---
+++ Q3_upperCaseString.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner; 
+    public class Q3_upperCaseString {
+
+    public static void main(String[]args){
+        Scanner src=new Scanner(System.in);
+        String name=src.nextLine();
+        System.out.print(name.toUpperCase());
+        src.close();
+    
+}
+    
+}
+
+
+/* Output:
+hello world
+HELLO WORLD
+ */
+    
+
