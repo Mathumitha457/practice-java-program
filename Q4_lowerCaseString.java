ba5c91bd6b5486724229cf0c0431ab3d7f1888e8
Mathumitha457 <s.mathumitha457@gmail.com>
2025-10-03 11:21:55 +0530

    Add files via upload

---
+++ Q4_lowerCaseString.java
@@ -0,0 +1,19 @@
+ import java.util.Scanner; 
+ public class Q4_lowerCaseString{ 
+ public static void main(String[]args){
+        Scanner src=new Scanner(System.in);
+        String name=src.nextLine();
+        System.out.print(name.toLowerCase());
+        src.close();
+    
+}
+    
+}
+
+
+/*Output:
+HELLO WORLD
+hello world 
+ */
+    
+
